867e135e65a263489284a4fc1eac4ee087d5897d
AndryYu <andryyu5419@163.com>
2018-09-07 20:55:54 +0800

    重新整理项目框架

---
+++ app/src/main/java/com/yufei/gadget/MainApplication.java
@@ -0,0 +1,12 @@
+package com.yufei.gadget;
+
+import com.android.yufei.baselibrary.BaseApplication;
+
+public class MainApplication extends BaseApplication{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ baselibrary/src/main/java/com/android/yufei/baselibrary/BaseApplication.java
@@ -0,0 +1,13 @@
+package com.android.yufei.baselibrary;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
